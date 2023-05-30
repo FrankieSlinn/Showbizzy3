b1af899a475393040900f06e27a314caa554689a
Franziska Kissling <frankissling@yahoo.co.uk>
2023-05-30 16:14:50 +0100

    Remove dupication in views, move registration links to top right

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,9 @@ class PagesController < ApplicationController
     def home
         @shows = Show.all # Assuming you have a Show model representing your shows
       end
+      def splash
+
+        @shows = Show.all # Assuming you have a Show model representing your shows
+      end
 
 end
