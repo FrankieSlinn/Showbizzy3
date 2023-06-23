ad2d421e146c28b801c4540c0d6295ebcfb99049
Franziska Kissling <frankissling@yahoo.co.uk>
2023-06-23 18:51:00 +0100

    confirmation email works

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -161,6 +161,8 @@ Devise.setup do |config|
   # unconfirmed_email column, and copied to email column on successful confirmation.
   config.reconfirmable = true
 
+
+
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
 
