ac4f5e865bbe07ecf1c4075e06ace6c213ca04b0
Franziska Kissling <frankissling@yahoo.co.uk>
2023-07-16 11:55:41 +0100

    force ssl margin login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,10 +43,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  # config.action_cable.allowed_request_origins = [ "http:/a0897205d292/example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
